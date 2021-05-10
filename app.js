472bd45d081034155e5e770a70b11c2369c7e8c9
nagesh <nageshbabu921@gmail.com>
2021-05-10 12:52:16 +0530

    lc

--- app.js
+++ app.js
@@ -12,12 +12,8 @@ document.onreadystatechange=()=>{
           testDiv.style.color='yellow';
 
         },3000)
-        const preloader = document.querySelector(".preloader");
 
-window.addEventListener("load", function () {
-  preloader.classList.add("hide-preloader");
-});
-
+        
             
     }
 }
